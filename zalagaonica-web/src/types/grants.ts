503112ce09b87581b1d88668bcda179766280ae7
Antonio Halužan <ahaluzan@playnirvana.com>
2025-11-20 13:14:40 +0100

    feat(auth): define player info grants

---
+++ zalagaonica-web/src/types/grants.ts
@@ -0,0 +1,11 @@
+export const grants = [
+  'player_info_personal',
+  'player_info_account',
+  'player_info_communication',
+  'player_info_government',
+  'player_info_documents',
+  'player_info_responsiblegaming',
+  'player_info_pep',
+  'player_info_changes',
+  'player_info_comments',
+];
